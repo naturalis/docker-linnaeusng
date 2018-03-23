d8e578bb3a8e35a7be73ec74069588aa3f8d4800
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2018-03-23 14:53:30 +0100

    updated docker-compose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,9 +9,11 @@ git clone -b php7-phpstorm-inspection --single-branch git@github.com:naturalis/l
 /bin/sed -i -E "s/'password' => '.*/'password' => '$MYSQL_PASSWORD',/" /var/www/html/configuration/app/configuration.php
 /bin/sed -i -E "s/'user' => '.*/'user' => '$MYSQL_USER',/" /var/www/html/configuration/app/configuration.php
 /bin/sed -i -E "s/'host' => '.*/'host' => '$MYSQL_HOST',/" /var/www/html/configuration/app/configuration.php
+/bin/sed -i -E "s/'tablePrefix' => '.*/'tablePrefix' => '$TABLE_PREFIX',/" /var/www/html/configuration/app/configuration.php
 /bin/sed -i -E "s/'password' => '.*/'password' => '$MYSQL_PASSWORD',/" /var/www/html/configuration/admin/configuration.php
 /bin/sed -i -E "s/'user' => '.*/'user' => '$MYSQL_USER',/" /var/www/html/configuration/admin/configuration.php
 /bin/sed -i -E "s/'host' => '.*/'host' => '$MYSQL_HOST',/" /var/www/html/configuration/admin/configuration.php
+/bin/sed -i -E "s/'tablePrefix' => '.*/'tablePrefix' => '$TABLE_PREFIX',/" /var/www/html/configuration/admin/configuration.php
 
 # create dirs and Set permissions
 mkdir -p /var/www/html/www/admin/templates/templates_c
