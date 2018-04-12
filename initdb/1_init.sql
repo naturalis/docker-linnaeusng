278e0b33b63acfc2389f25dd312d65e751b98ec4
Joep Vermaat <joep@joep-i.nl>
2018-04-12 13:57:28 +0200

    Verder werkend krijgen

--- initdb/1_init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE linnaeus_ng ;
-GRANT ALL PRIVILEGES ON linnaeus_ng.* TO 'linnaeus_user'@'%' IDENTIFIED BY 'PASSWORD';
-GRANT ALL PRIVILEGES ON linnaeus_ng.* TO 'linnaeus_user'@'localhost' IDENTIFIED BY 'PASSWORD';
-
